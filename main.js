45e2d8324f5e87d674ffcb0e2e6fdda1f46e9fd5
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-03-20 14:20:32 +0400

    Commented code. Cookies added

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // 'use strict';
 
-var express = require ('express');
-var  app = express ();
+const express = require ('express');
+const  app = express ();
 const  fs = require('fs');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-
+const session=require('express-session');
 const NodeCouchDb = require('node-couchdb');
-
+const FileStore=require('session-file-store')(session);
+const uuid=require('uuid/v4');
 const COOKIE = "chipsAhoy";
 
 const couch = new NodeCouchDb();
@@ -16,21 +16,41 @@ const couch = new NodeCouchDb();
 let User = require('./db.js');
 
 
-
-const saltRounds = 10;
-
 app.set ('port' , process.env.PORT || 3005);
+
 app.use ( express.static ( __dirname));
+
+/**
+ * Middleware to parse requests received and parse cookie
+ */
 app.use(cookieParser());
+
+/**
+ * Middleware to handle user session and cookies
+ */
+app.use(session({
+    genid: (req) => {
+      return uuid() // use UUIDs for session IDs
+    },
+    secret: COOKIE,
+    store: new FileStore(),
+    cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: true
+  }));
 
 var urlencodedParser = bodyParser.urlencoded({ extended:false });
 
-// landing page (currently login)
+/**
+ * Default Directory.
+ * Login page if not signed in 
+ * Homepage if signed in
+ */
 app . get ( '/' , function ( req , res ){
     res . set ( 'Content-Type' , 'text/html' );
-    // check cookies
-    // if no cookies, send login page
-    if (req.cookies.COOKIE)
+    var sess=req.session;
+
+    if(sess.username)
     fs.readFile('public/homepage.html', function(err, data){
       if (err) throw err;
       else {
@@ -45,7 +65,9 @@ app . get ( '/' , function ( req , res ){
     })
 });
 
-// send sign up page on request
+/**
+ * Send signup page on request
+ */
 app . get ( '/signup' , function ( req , res ){
     res . set ( 'Content-Type' , 'text/html' );
     fs.readFile('public/signup.html', function(err, data){
@@ -56,16 +78,14 @@ app . get ( '/signup' , function ( req , res ){
     })
 });
 
-// Login GET request
+/**
+ * Serve login attempt as a GET request. Redirect to homepage if authenticated
+ */
 app.get('/attemptLogin', function(req, res){
-    var credentials= {
-        username:req.query.username,
-        password:req.query.password
-    }
     var user = new User(req.query.username, req.query.username, req.query.password);
     res.set('Content-Type','text/plain');
     user.authenticate().then((code)=>{
-            res.cookie(COOKIE, credentials.username, {maxAge: 3600000, secure: true});
+            req.session.username=code;
             res.send('true');
     }, (error)=>{
       res.status(500);
@@ -73,7 +93,9 @@ app.get('/attemptLogin', function(req, res){
      })
 });
 
-// Register POST request (post for security)
+/**
+ * 
+ */
 app.post('/attemptRegister',urlencodedParser, function(req, res){
 
      var newUser = new User(req.body.email, req.body.username, req.body.password);
@@ -121,77 +143,4 @@ app.listen (app.get( 'port' ), function (){
     console.log ('Express started on http://localhost:' +
     app.get ('port') + '; press Ctrl-C to terminate.' );
 });
-//
-//
-// /**
-// *Class name: User
-// *Attributes: username,password,email
-// *Methods:
-//             generateid(): Creates a unique id for the document before registration (Called within register function)
-//             register(): Registers the user with given details
-// */
-// function user(username, password, email) {
-//     this.username = username;
-//     this.password = password;
-//     this.email = email;
-// }
-//
-// user.prototype.register = function (err) {
-//
-//     if (err == 0) {
-//         couch.insert("user-info", {
-//             _id: this.id,
-//             username: this.username,
-//             password: this.password,
-//             email: this.email
-//         }).then(({ data, header, status }) => {
-//             console.log(data);
-//             console.log(header);
-//             console.log(status);
-//         }, err => {
-//             console.log("Error" + err);
-//         });
-//
-//
-//     }
-//     else if (err == 1) {
-//         //email already exists
-//     }
-//     else if (err == 2) {
-//         //username already exists
-//     }
-//
-// }
-//
-//
-// //Function to check if user with given email or username exists and passes 1/2/0 to the callback function
-// //if user with given email exists/username exists/does not exist
-//
-// user.prototype.checkifExists = function (res, callback) {
-//     const view_url = "/_design/get-user-email/_view/useremail";
-//     const option = { key: this.email };
-//     couch.get(u_db, view_url, option).then(({ data, header, status }) => {
-//         console.log("\n\n\n\n\nData:" + JSON.stringify(data));
-//         if (data.rows.length > 0)
-//             callback(1,data.rows[0].password);
-//         else {
-//             couch.get(u_db, "/_design/get-user-username/_view/user-username", { key: this.username }).then(({ data, header, status }) => {
-//                 if (data.rows.length > 0)
-//                     callback(2,data.rows[0].password);
-//                 else
-//                     callback(0);
-//             }, err => {
-//                 console.log("Error: " + err);
-//             });
-//         }
-//
-//     }, err => {
-//         console.log("Error: " + err);
-//     });
-// }
-//
-// user.prototype.generateid = function () {
-//     var ids;
-//     couch.uniqid().then(ids => ids[0]);
-//     this.id = ids;
-// }
+
